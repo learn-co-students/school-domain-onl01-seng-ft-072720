075e832a1e3137e3000f520f963087d7034f2f0c
Tyler Huffman <ty.huff24@gmail.com>
2020-08-04 14:52:14 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,33 @@
+require 'pry'
 # code here!
+class School
+  attr_accessor :name, :roster
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(name, grade)
+    @roster[grade]||=[]
+    @roster[grade] << name
+  end
+
+
+def grade(grade)
+  @roster[grade]
+end
+
+def sort
+
+  @roster.map do|grade, students|
+    students.sort! do |one, two|
+      one <=> two
+    #  binding.pry
+end
+end
+@roster
+end
+
+
+end
