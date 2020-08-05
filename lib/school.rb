24edf5e1a43b8edbdbc4323cde7dc5c21102f076
Virgil Cantu <cantu.sampsom@gmail.com>
2020-08-05 18:28:38 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,30 @@
-# code here!
+require 'pry'
+
+class School
+attr_accessor :roster
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(student_name, grade)
+    if @roster[grade] == nil
+    @roster[grade] = []
+    @roster[grade] << student_name
+  else
+    @roster[grade] << student_name
+    end
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    @roster.each do |grade, students|
+        students.sort!
+        students
+    end
+  end
+end
