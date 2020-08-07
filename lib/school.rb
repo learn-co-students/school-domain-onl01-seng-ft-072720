a411bfc2cdc59ad50233e096931aedaa5bacf3ea
Hyeoncheol Jung <joe.hyeoncheol.jung@gmail.com>
2020-08-07 02:48:09 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,36 @@
-# code here!
+class School
+  
+  attr_accessor :student, :roster
+  
+  def initialize(roster)
+    @roster = {}
+  
+  end
+  
+  attr_writer :add_student
+    
+  def add_student(name, grade)
+    @roster[grade] ||= []
+    @roster[grade] << name
+  end
+  
+  def grade(grade)
+    @roster[grade]
+  end
+  
+  def sort
+    @roster.each do |grade, name|
+      name.sort!
+  end
+
+  @roster
+end
+end
+
+#       @school.add_student("Jack Bauer", 7)
+
+#       # key order does not matter; this is testing that the students in each respective value are in alphabetical order
+#       expect(@school.sort).to eq({7 => ["Blake Johnson", "Jack Bauer"], 9 => ["Bart Simpson", "Homer Simpson"], 10 => ["Avi Flombaum", "Jeff Baird"]})
+#     end
+#   end
+# end
