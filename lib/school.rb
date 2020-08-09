5afd3d4e78ee0b9116329abff6ccca9b702a0968
Christie Yoon <cleex1999@gmail.com>
2020-08-09 02:25:45 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,36 @@
-# code here!
+require "pry"
+class School
+
+    def initialize(school_name)
+      @school_name = school_name
+      @roster = {}
+    end
+
+    def roster
+      @roster
+    end
+
+    def add_student(name, grade)
+        @student_name = name
+        @grade = grade
+      if @roster.include?(grade) == false
+         @roster[grade] = []
+      end
+        @roster[grade] << name
+    end
+
+    def grade(grade)
+      @roster[grade]
+    end
+
+    def sort
+      @roster.each do |grade, name|
+         @roster[grade] = name.sort
+      end
+    end
+  end
+
+#school = School.new("Bayside High School")
+#school.roster
+#school.add_student("Zach Morris", 9)
+#school.roster
